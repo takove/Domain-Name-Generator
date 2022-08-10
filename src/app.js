f0aecd558db009219d006782a5d7810a8a5aaa4b
Luis Rosal <luiss.rosalg.13@gmail.com>
2022-08-10 20:23:20 +0000

    Domain Name Generator with JavaScript

--- src/app.js
+++ src/app.js
@@ -6,6 +6,21 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const h1 = document.querySelector("#domainGenerator");
+  let pronoun = ["the", "our", "your"];
+  let adj = ["great", "big", "awesome"];
+  let noun = ["jogger", "raccoon", "page"];
+  let domainName;
+  let domainNameArr = [];
+  for (let pronounIndex = 0; pronounIndex < pronoun.length; pronounIndex++) {
+    for (let adjIndex = 0; adjIndex < adj.length; adjIndex++) {
+      for (let nounIndex = 0; nounIndex < noun.length; nounIndex++) {
+        domainName = `${pronoun[pronounIndex]}${adj[adjIndex]}${noun[nounIndex]}.com`;
+
+        domainNameArr.push(domainName);
+        console.log(domainName);
+      }
+    }
+  }
+  h1.innerHTML = domainNameArr;
 };
